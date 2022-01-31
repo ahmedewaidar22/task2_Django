c12beedf7e6912cd864d9316ac436881c5b9d799
ahmedewaidar22 <ahmedewaidar22@gmail.com>
2022-01-31 23:13:38 +0200

    uploding files

---
+++ day2/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from new.views import*
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('signup/', signup,name="signup"),
+    path('',nav,name="nav"),
+    path('login/',login,name="login"),
+    path('delete/',delete,name="delete"),
+    path('insertstudent/',insertstudent,name="insertstudent"),
+    path('selectall/',selectall,name="selectall"),
+    path('selectbyname/',selectbyname,name="selectbyname"),
+    path('update/',update,name="update"),
+    path('homenav/',homenav,name="homenav"),
+
+]
---
+++ new/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect
+from .models import Intake
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+def nav(request):
+    return render(request, 'nav.html')
+def delete(request):
+    if (request.method == 'GET'):
+        return render(request, 'delete.html')
+    else:
+        context = {}
+        id = request.POST['id']
+
+        Intake.objects.filter(id=id).delete()
+        intakes = Intake.objects.all()
+        context['intakes'] = intakes
+        return render(request, 'delete.html',context)
+def insertstudent(request):
+    if (request.method == 'GET'):
+        return render(request, 'insertstudent.html')
+    else:
+        username = request.POST['username']
+        email = request.POST['email']
+        password =request.POST['password']
+        Intake.objects.create(username=username, email=email, password=password)
+    return render(request, 'insertstudent.html')
+
+def selectbyname(request):
+    context={}
+    if (request.method == 'GET'):
+        return render(request, 'selectbyname.html')
+    else:
+         username = request.POST['username']
+         intakes =Intake.objects.filter(username=username)
+         context['intakes'] = intakes
+         return render(request, 'selectbyname.html',context)
+def selectall(request):
+    context = {}
+    intakes = Intake.objects.all()
+    context['intakes'] = intakes
+    return render(request, 'selectall.html',context)
+def update(request):
+    return render(request, 'update.html')
+def homenav(request):
+    return render(request, 'homenav.html')
+
+def signup(request):
+    if (request.method == 'GET'):
+        return render(request, 'signup.html')
+    else:
+        username = request.POST['username']
+        email = request.POST['email']
+        password =request.POST['password']
+        Intake.objects.create(username=username, email=email, password=password)
+        return  redirect("login")
+def login(request):
+    if (request.method == 'GET'):
+        return render(request, 'login.html')
+    else:
+
+        email = request.POST['email']
+        password = request.POST['password']
+        intakes=Intake.objects.all()
+        for intake in intakes:
+
+            if intake.email==email and intake.password==password:
+                return  redirect("homenav")
+            else:
+                return render(request, "login")
+
+
+
+
+
