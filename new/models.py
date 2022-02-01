afb566a8a6ab8687d1a3961fb4f4744daaf5d597
ahmedewaidar22 <ahmedewaidar22@gmail.com>
2022-02-01 18:51:04 +0200

    session &superusers

--- new/models.py
+++ new/models.py
@@ -6,3 +6,11 @@ class Intake(models.Model):#ORM
     username=models.CharField(max_length=30)
     email=models.EmailField()
     password=models.CharField(max_length=70)
+
+class Track(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Trainee(models.Model):
+    name = models.CharField(max_length=40)
+    track_name = models.ForeignKey(Track, on_delete=models.CASCADE)
--- new/views.py
+++ new/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from .models import Intake
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
-
 def nav(request):
     return render(request, 'nav.html')
 def delete(request):
@@ -52,20 +51,27 @@ def signup(request):
         username = request.POST['username']
         email = request.POST['email']
         password =request.POST['password']
+
         Intake.objects.create(username=username, email=email, password=password)
+        if request.POST.get('check',True):
+            User.objects.create_user(username=username,password=password,is_staff=True)
+        else:
+            User.objects.create_user(username=username, password=password, is_staff=False)
+
         return  redirect("login")
 def login(request):
     if (request.method == 'GET'):
         return render(request, 'login.html')
     else:
 
-        email = request.POST['email']
+        username = request.POST['username']
         password = request.POST['password']
-        intakes = Intake.objects.all().filter(email=email, password=password)
+        intakes = Intake.objects.all().filter(username=username, password=password)
 
         for intake in intakes:
 
-            if intake.email==email and intake.password==password:
+            if intake.username==username and intake.password==password:
+                request.session['username']= username
                 return  redirect("homenav")
             else:
                 return render(request, "login.html")
